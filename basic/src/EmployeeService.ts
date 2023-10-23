b310e7e4f4ad2bdb7ff55d922c0810d2f6d03e32
Diego Alfonso Prieto Torres <da.prieto1@uniandes.edu.co>
2023-10-23 11:20:00 -0500

    feat: add server

---
+++ basic/src/EmployeeService.ts
@@ -0,0 +1,34 @@
+import {EmployeeServiceHandlers} from "../proto/employees/EmployeeService";
+import {
+    sendUnaryData,
+    ServerDuplexStream,
+    ServerReadableStream,
+    ServerUnaryCall,
+    ServerWritableStream,
+    UntypedHandleCall
+} from "@grpc/grpc-js";
+import {AddPhotoRequest__Output} from "../proto/employees/AddPhotoRequest";
+import {AddPhotoResponse} from "../proto/employees/AddPhotoResponse";
+import {GetAllRequest__Output} from "../proto/employees/GetAllRequest";
+import {EmployeeRequest__Output} from "../proto/employees/EmployeeRequest";
+import {EmployeeResponse} from "../proto/employees/EmployeeResponse";
+import {GetByBadgeNumberRequest__Output} from "../proto/employees/GetByBadgeNumberRequest";
+
+export class EmployeeService implements EmployeeServiceHandlers {
+    [name: string]: UntypedHandleCall;
+
+    AddPhoto(call: ServerReadableStream<AddPhotoRequest__Output, AddPhotoResponse>, callback: sendUnaryData<AddPhotoResponse>): void {
+    }
+
+    GetAll(call: ServerWritableStream<GetAllRequest__Output, EmployeeResponse>): void {
+    }
+
+    GetByBadgeNumber(call: ServerUnaryCall<GetByBadgeNumberRequest__Output, EmployeeResponse>, callback: sendUnaryData<EmployeeResponse>): void {
+    }
+
+    Save(call: ServerUnaryCall<EmployeeRequest__Output, EmployeeResponse>, callback: sendUnaryData<EmployeeResponse>): void {
+    }
+
+    SaveAll(call: ServerDuplexStream<EmployeeRequest__Output, EmployeeResponse>): void {
+    }
+}
